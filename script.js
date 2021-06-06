559102e8e7eed25540757a46bf531f5d7dffa0b3
Ant1gen <antigen88@gmail.com>
2021-06-06 12:01:26 +0300

    Removed game function, added endgame function which checks and declares the winner, added buttons and eventlistener for them

--- script.js
+++ script.js
@@ -1,66 +1,74 @@
 //rock paper scissors
+let playerScore = 0;
+let computerScore = 0;
+let playerScoreDOM = document.getElementById('playerScore');
+let computerScoreDOM = document.getElementById('computerScore');
+let resultDOM = document.getElementById('result');
+let bottons = document.querySelectorAll('.button');
+
+bottons.forEach(button => button.addEventListener('click', playRound));
+
 function computerPlay()
 {
+    // a random number from 1 to 3
     let a = Math.floor((Math.random()*9)%3)+1;
     if (a == 1){
-        return "Rock";
+        return "rock";
     }
     else if (a == 2){
-        return "Paper";
+        return "paper";
     }
     else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
-function playRound (playerSelection, computerSelection)
+function playRound ()
 {
-    if (playerSelection.toUpperCase() == computerSelection.toUpperCase())
+    let playerSelection = this.id;
+    let computerSelection = computerPlay();
+    if (playerSelection == computerSelection)
     {
+        playerScoreDOM.innerText = 'Player Score:' + playerScore;
+        computerScoreDOM.innerText = 'Computer Score:' +computerScore;
+        resultDOM.innerText = "Draw";
         return "Draw";
     }
-    else if (playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "SCISSORS" ||
-        playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "ROCK" ||
-        playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "PAPER")
-            {
-                return "Win";
-            }
-    else 
-        {
-        return "Lost";
-        }
-}
-let computerSelection = computerPlay();
-//let playerInput = "rock";
-
-function game()
-{
-playerScore = 0;
-computerScore = 0;
-    for (i=0; i <5; i++)
-    {
-        let playerInput = window.prompt("Chose Rock Paper or Scissors");
-        let roundResult = playRound(playerInput,computerPlay());
-        console.log(roundResult);
-        if ( roundResult == "Win")
+    else if (playerSelection == "rock" && computerSelection == "scissors" ||
+        playerSelection == "paper" && computerSelection == "rock" ||
+        playerSelection == "scissors" && computerSelection == "paper")
         {
             playerScore += 1;
+            playerScoreDOM.innerText = 'Player Score:' + playerScore;
+            computerScoreDOM.innerText = 'Computer Score:' +computerScore;
+            resultDOM.innerText = "Win";
+            if (endGame()){return;}
+            return "Win";
         }
-        else if ( roundResult == "Lost")
+    else 
         {
             computerScore += 1;
+            playerScoreDOM.innerText = 'Player Score:' + playerScore;
+            computerScoreDOM.innerText = 'Computer Score:' +computerScore;
+            resultDOM.innerText = "Lost";
+            if (endGame()){return;}
+            return "Lost";
         }
-    }
-    if (playerScore > computerScore)
+}
+
+function endGame (){
+    if (playerScore == 5 )
     {
-        return "Player Win";
-    }
-    else if (playerScore < computerScore)
+        playerScore = 0;
+        computerScore = 0;
+        resultDOM.innerText = "You Win the Game";
+        return true;
+    } else if (computerScore == 5)
     {
-        return "Computer Win";
+        playerScore = 0;
+        computerScore = 0;
+        resultDOM.innerText = "You Lost the Game";
+        return true;
     }
-    else 
-    { 
-        return "Draw"
-    }
+    return false;
 }
